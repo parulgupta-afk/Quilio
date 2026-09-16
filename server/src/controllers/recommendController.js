768cc8f5e99cd49b570422ee195875fbef8be7ba
Parul Gupta <parulmahajan863@gmail.com>
2026-09-16 23:10:27 +0530

    fix: add missing @google/generative-ai dependency for Gemini AI features

--- server/src/controllers/recommendController.js
+++ server/src/controllers/recommendController.js
@@ -56,7 +56,7 @@ const getSimilarPosts = async (req, res) => {
 
     // Get all other published posts that have embeddings
     const allChunks = await EmbeddingChunk.aggregate([
-      { $match: { post: { $ne: require('mongoose').Types.ObjectId.createFromHexString(postId) } } },
+      { $match: { post: { $ne: new (require('mongoose').Types.ObjectId)(postId) } } },
       {
         $group: {
           _id: '$post',
